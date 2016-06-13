04b18bb1f153a9c490d8c4c81a04873d28ca38ef
yeser07 <yeser07@gmail.com>
2016-06-13 11:16:02 -0600

    Cambios en Main
    se realizan cambios en Main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <iostream>
+#include<curso.h>
+#include<clase.h>
+#include<laboratorio.h>
+#include<lista.h>
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -11,4 +15,5 @@ int main(int argc, char *argv[])
     cout<<"Hello World"<<endl;
 
     return a.exec();
+
 }
